7076fb6f7a71f1d03dd971424a64f5ba3efee223
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2019-08-04 12:40:54 +0900

    Implement the Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -39,40 +39,61 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            new Align(
-              alignment: Alignment.centerLeft,
+            new Expanded(
               child:
+              new Container(
+                color: Color.fromARGB(255, 255, 255, 0),
+                child:
                 new Text(
-                  "Left",
+                  "First item",
                   style: new TextStyle(fontSize:32.0,
                       color: const Color(0xFF000000),
                       fontWeight: FontWeight.w400,
                       fontFamily: "Roboto"),
-                )
-            ),
+                ),
 
-            new Align(
+                padding: const EdgeInsets.all(0.0),
                 alignment: Alignment.center,
-                child:
-                new Text(
-                  "Center",
-                  style: new TextStyle(fontSize:32.0,
-                      color: const Color(0xFF000000),
-                      fontWeight: FontWeight.w400,
-                      fontFamily: "Roboto"),
-                )
+              ),
+
             ),
 
-            new Align(
-                alignment: Alignment.centerRight,
+            new Expanded(
+              child:
+              new Container(
+                color: Color.fromARGB(255, 255, 125, 0),
                 child:
                 new Text(
-                  "Right",
+                  "Second item",
                   style: new TextStyle(fontSize:32.0,
                       color: const Color(0xFF000000),
                       fontWeight: FontWeight.w400,
                       fontFamily: "Roboto"),
-                )
+                ),
+
+                padding: const EdgeInsets.all(0.0),
+                alignment: Alignment.center,
+              ),
+
+            ),
+
+            new Expanded(
+              child:
+              new Container(
+                color: Color.fromARGB(255, 255, 0, 0),
+                child:
+                new Text(
+                  "Three item",
+                  style: new TextStyle(fontSize:32.0,
+                      color: const Color(0xFF000000),
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Roboto"),
+                ),
+
+                padding: const EdgeInsets.all(0.0),
+                alignment: Alignment.center,
+              ),
+
             )
           ]
 
