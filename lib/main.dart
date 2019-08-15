87c1048de08e58f3bbf5059dff411b4666c3b071
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2019-08-16 00:36:24 +0900

    Implement the Cupertino UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Generated App',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: const Color(0xFF2196f3),
-        accentColor: const Color(0xFF2196f3),
-        canvasColor: const Color(0xFFfafafa),
-      ),
       home: new MyHomePage(),
     );
   }
@@ -27,58 +22,189 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List _items = <Widget>[];
-  String _message;
-  int _tapped = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _message = 'OK?';
-    for(var i = 0; i < 5; i++){
-      var item = ListTile(
-        leading: const Icon(Icons.android),
-        title: Text('No, $i'),
-        onTap: (){
-          _tapped = i;
-          tapItem();
-        },
-      );
-      _items.add(item);
-    }
-  }
+  String _message = 'OK?';
+  bool _switch = true;
+  double _slider = 0.0;
+  int _modal = 0;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Color.fromARGB(255, 235, 235, 235),
       appBar: new AppBar(
         title: new Text('App Name'),
       ),
 
-      body: Center(
-        child: Text(
-          _message,
-          style: TextStyle(
-            fontSize: 32.0,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            _message,
+            style: TextStyle(
+              fontSize: 32.0,
+              color: const Color(0xFF000000),
+              fontWeight: FontWeight.w400,
+              fontFamily: "Roboto"
+            ),
           ),
-        ),
+          
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+          ),
+          
+          CupertinoSwitch(
+            value: _switch,
+            onChanged: (bool value){
+              print('switch.');
+              setState(() {
+                _switch = value;
+                _message = 'switch: $_switch';
+              });
+            },
+          ),
+          
+          CupertinoSlider(
+            value: _slider,
+            min: 0.0,
+            max: 1.0,
+            divisions: 100,
+            onChanged: (double value){
+              print('slider.');
+              setState(() {
+                _slider = value;
+                _message = 'slider: $_slider';
+              });
+            },
+          ),
+          
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: CupertinoButton(
+              borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+              pressedOpacity: 0.5,
+              color: CupertinoColors.activeBlue,
+              onPressed: buttonPressed,
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "Tap me!",
+                style: TextStyle(
+                  fontSize: 28.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Roboto"
+                ),
+              ),
+            ),
+          )
+        ],
       ),
 
-      drawer: Drawer(
-        child: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(20.0),
-          children: _items,
+      bottomNavigationBar: CupertinoNavigationBar(
+        leading: Icon(CupertinoIcons.left_chevron),
+        middle: Text('Navigation'),
+        trailing: IconButton(
+          icon: Icon(CupertinoIcons.right_chevron),
+          onPressed: showPicker,
         ),
       ),
 
     );
   }
 
-  void tapItem(){
-    Navigator.pop(context);
+  void buttonPressed(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text("Hello!"),
+        content: const Text("This is sample."),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            child: const Text('Cancel'),
+            onPressed: () => Navigator.pop<String>(context, 'Cancel'),
+          ),
+          CupertinoDialogAction(
+            child: const Text('OK'),
+            onPressed: () => Navigator.pop<String>(context, 'OK'),
+          )
+        ],
+      )
+    ).then<void>((value) => resultAlert(value));
+  }
+  
+  void resultAlert(String value){
     setState(() {
-      _message = 'tapped: [$_tapped]';
+      _message = 'selected: $value';
     });
+  }
+  
+  void showPicker(){
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context){
+        return CupertinoPicker(
+          backgroundColor: CupertinoColors.black,
+          itemExtent: 50.0,
+          children: <Widget>[
+            Text(
+              'One',
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.white
+              ),
+            ),
+            Text(
+              'Two',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white
+              ),
+            ),
+            Text(
+              'Three',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white
+              ),
+            ),
+            Text(
+              'Four',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white
+              ),
+            ),
+            Text(
+              'Five',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white
+              ),
+            ),
+            Text(
+              'Six',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white
+              ),
+            ),
+          ],
+          onSelectedItemChanged: (int value){
+            print('modal.');
+            setState(() {
+              _modal = value;
+              _message = 'modal: $_modal';
+            });
+          },
+        );
+      },
+    );
   }
 }
